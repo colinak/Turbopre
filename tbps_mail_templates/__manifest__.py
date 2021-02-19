b1d54918e72a24f79c32fbab9af9d0ef0408369a
Kewitz Colina <kewitzcolina@gmail.com>
2021-02-19 09:12:01 -0400

    [ADD] Modulos: Theme Enterprise, debrand y email template.

---
+++ tbps_mail_templates/__manifest__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Tbps Mail Template",
+    'version': "14.0.1",
+    'author': "Kewitz Colina",
+    'website': "https://github.com/colinak",
+    'summary': "Mail Template Turbopre",
+    'sequence': 10,
+    'description': "Mail Template Turbopre",
+    'category': "mail",
+    'depends': ['base', 'auth_signup'],
+    'data': [
+        'data/auth_signup_data_tbps.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
+
