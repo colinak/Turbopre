b382ed4380f59f1129b8d003517d980fe73bc00a
Kewitz Colina <kewitzcolina@gmail.com>
2021-03-09 13:04:08 -0400

    [ADD] Plantilla de email para resetear password.

---
+++ tbps_mail_templates/models/res_users.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.addons.base.models.ir_mail_server import MailDeliveryException
+from odoo.addons.auth_signup.models.res_partner import SignupError, now
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    def action_reset_password(self):
+        """ create signup token for each user, and send their signup url by email """
+        if self.env.context.get('install_mode', False):
+            return
+        if self.filtered(lambda user: not user.active):
+            raise UserError(_("You cannot perform this action on an archived user."))
+        # prepare reset password signup
+        create_mode = bool(self.env.context.get('create_user'))
+
+        # no time limit for initial invitation, only for reset password
+        expiration = False if create_mode else now(days=+1)
+
+        self.mapped('partner_id').signup_prepare(signup_type="reset", expiration=expiration)
+
+        # send email to users with their signup url
+        template = False
+        if create_mode:
+            try:
+                template = self.env.ref('tbps_mail_templates.inherit_set_password_email', raise_if_not_found=False)
+            except ValueError:
+                pass
+        if not template:
+            template = self.env.ref('tbps_mail_templates.inherit_reset_password_email')
+        assert template._name == 'mail.template'
+
+        template_values = {
+            'email_to': '${object.email|safe}',
+            'email_cc': False,
+            'auto_delete': True,
+            'partner_to': False,
+            'scheduled_date': False,
+        }
+        template.write(template_values)
+
+        for user in self:
+            if not user.email:
+                raise UserError(_("Cannot send email: user %s has no email address.", user.name))
+            # TDE FIXME: make this template technical (qweb)
+            with self.env.cr.savepoint():
+                force_send = not(self.env.context.get('import_file', False))
+                template.send_mail(user.id, force_send=force_send, raise_exception=True)
+            _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
