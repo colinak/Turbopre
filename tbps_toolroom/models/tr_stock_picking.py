861f24799de9f78d1a9e4d09c1bebf723ed1317a
Kewitz Colina <kewitzcolina@gmail.com>
2025-04-02 11:36:39 -0400

    [UPD] Validaciones varias.

--- tbps_toolroom/models/tr_stock_picking.py
+++ tbps_toolroom/models/tr_stock_picking.py
@@ -260,8 +260,11 @@ class TrStockPicking(models.Model):
     def check_duplicate_records(self):
         viewed = set()
         for line in self.move_line_ids:
-            key = line.lot_name
-            if line.lot_name in viewed:
+            if self.picking_type_code == 'assignment' or self.picking_type_code == 'loans' and line.lot_id.stage != 'available':
+                raise UserError(f"¡Error! \nEsta herramienta {line.lot_name} ya se encuentra prestada")
+            elif self.picking_type_code == 'reception' and line.lot_id.stage == 'available':
+                raise UserError(f"¡Error! \nEsta herramienta {line.lot_name} no se encuentra prestada")
+            elif line.lot_name in viewed:
                 raise UserError(f"¡Error!\nSe consiguieron registros duplicados {line.lot_name}")
             else:
                 viewed.add(line.lot_name)
